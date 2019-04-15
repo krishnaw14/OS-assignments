4bd0ecdd2795351e71b1de1531caafa00d6eb1cd
krishnaw14 <krishnaw14@gmail.com>
2019-04-15 19:51:08 +0530

    Master worker with fixed sized buffer implemented

--- Assignment_3/master-worker-array.c
+++ Assignment_3/master-worker-array.c
@@ -10,19 +10,12 @@
 
 int item_to_produce;
 int total_items, max_buf_size, num_workers;
-// declare any global data structures, variables, etc that are required
-// e.g buffer to store items, pthread variables, etc
 
 int* buffer;
 int fill_index, use_index, count, requests_processed;
 pthread_mutex_t mutex; 
 pthread_cond_t fill; 
 pthread_cond_t empty; 
-
-// pthread_mutex_t mutex; = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t fill; = PTHREAD_COND_INITIALIZER;
-// pthread_cond_t empty; = PTHREAD_COND_INITIALIZER;
-
 
 void print_produced(int num) {
 
@@ -35,26 +28,15 @@ void print_consumed(int num, int worker) {
   
 }
 
-
-/* produce items and place in buffer (array or linked list)
- * add more code here to add items to the buffer (these items will be consumed
- * by worker threads)
- * use locks and condvars suitably
- */
 void *generate_requests_loop(void *data)
 {
   int thread_id = *((int *)data);
   int i;
 
-  // printf("Master Thread ID: %i\n", thread_id);
-
   for(i = 0; i< total_items; i++)
     {
-      // printf("Locked\n");
       pthread_mutex_lock(&mutex);
-      // printf("Inside mutex. item_to_produce: %i. total_items: %i \n", item_to_produce, total_items);
       while (count >= max_buf_size){
-      	// printf("going on wait!!!!!!!\n");
 	     pthread_cond_wait(&empty,&mutex);
       }
      
@@ -69,43 +51,41 @@ void *generate_requests_loop(void *data)
       
       pthread_cond_broadcast(&fill);
       pthread_mutex_unlock(&mutex);
-    }
-  
-  // return 0;
+    } 
 }
-
-//write function to be run by worker threads
-//ensure that the workers call the function print_consumed when they consume an item
 
 void *consume_requests_loop(void* data)
 {
-  // int i;
+  int thread_processes = 0;
   int thread_id = *((int *)data);
-  // printf("In Child Thread: %i. Total Items: %i \n", thread_id, total_items);
+  int max_processes_per_thread = (total_items - (num_workers-1)*total_items/num_workers);
 
-  while(requests_processed < total_items)
+  while(thread_processes < max_processes_per_thread && requests_processed < total_items)
   {
   	pthread_mutex_lock(&mutex);
-  	if (requests_processed==total_items){
-  		printf("Return thread %i \n", thread_id);
+
+  	while(count==0 && requests_processed<total_items){
+  		pthread_cond_wait(&fill, &mutex);
+  	}
+
+  	if (requests_processed>=total_items){
+  		pthread_cond_broadcast(&fill);
+  		pthread_mutex_unlock(&mutex);
   		return 0;
   	}
 
-  	while(count==0 && requests_processed!=total_items)
-  		pthread_cond_wait(&fill, &mutex);
-
   	int consumed_number = buffer[use_index];
-  	// printf("In Child: use_index: %i, buffer[use_index]: ", use_index, buffer[use_index]);
   	use_index = (use_index+1)%max_buf_size;
   	count--;
   	requests_processed++;
-
+  	thread_processes++;
   	print_consumed(consumed_number, thread_id);
-  	// printf("Consumed %d by worker %d. requests_processed: %i. use_index: %i \n", consumed_number, thread_id, requests_processed, use_index);
-  	pthread_cond_broadcast(&fill);
+
   	pthread_cond_signal(&empty);
   	pthread_mutex_unlock(&mutex);
   }
+
+  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -147,28 +127,23 @@ int main(int argc, char *argv[])
 
   //create master producer thread
   pthread_create(&master_thread, NULL, generate_requests_loop, (void *)&master_thread_id);
-  printf("Master Thread Created\n");
+  // printf("Master Thread Created\n");
 
-  //create worker consumer threads
-  // printf("Starting child creation\n");
   for(i = 0; i<num_workers; i++){
-    // printf("Worker Thread ID: %i\n", worker_thread_ids[i]);
-    check_error_variable = pthread_create(&worker_threads[i], NULL, consume_requests_loop, (void*) &worker_thread_ids[i]);
+    pthread_create(&worker_threads[i], NULL, consume_requests_loop, (void*) &worker_thread_ids[i]);
   }
-
 
   //wait for all threads to complete
   pthread_join(master_thread, NULL);
   printf("master joined\n");
 
   for(i = 0; i<num_workers; i++){
-  	// printf("Worker %i joining \n", worker_thread_ids[i]);
     pthread_join(worker_threads[i], NULL);
     printf("Worker %i joined \n", worker_thread_ids[i]);
   }
 
   //deallocate and free up any memory you allocated
-
+  free(buffer);
   pthread_exit(NULL);
   
   return 0;
